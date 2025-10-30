36cbcba9a400824a67813d422879e9abee126d43
hamzah <hamzahnafalah@gmail.com>
2025-10-30 16:11:19 +0700

    anatomy

--- src/Models/HeadToToe.php
+++ src/Models/HeadToToe.php
@@ -5,4 +5,11 @@ namespace Hanafalah\ModuleAnatomy\Models;
 class HeadToToe extends Anatomy
 {
     protected $table = 'unicodes';
+
+    protected static function booted(): void{
+        parent::booted();
+        static::addGlobalScope('flag',function($query){
+            $query->where('flag','HeadToToe');
+        });
+    }
 }
